3132d3358cc635b9633daf960dc3d468837d64a6
Alejandro <Alejandro.perezsanabria@dcmail.ca>
2025-03-27 22:35:48 -0400

    skeleton for application

---
+++ main.py
@@ -0,0 +1,29 @@
+import discord
+import os
+from dotenv import load_dotenv
+# intents are needed for teh client section
+# so here we add if this supposed to message join chat and more
+# Create intents
+intents = discord.Intents.default()
+intents.message_content = True  # Enable message content intent if needed
+
+# loading the .env token since there is a bug
+load_dotenv()
+TOKEN = os.getenv("TOKEN")
+
+# discord bots listen for event and then reacts to it
+client = discord.Client(intents=intents)
+#log in event
+@client.event
+async def on_ready():
+    print('Bot logged in as {0.user}'.format(client))
+#on message even
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+client.run(TOKEN)
