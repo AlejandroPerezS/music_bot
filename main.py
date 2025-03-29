06947cf7e4f5e9fe8dbfbeaecfecb98d0f3e97b5
huntyboy102 <hrlovering@hotmail.com>
2025-03-28 23:59:17 -0400

    Added Spotify but not for discord...

--- main.py
+++ main.py
@@ -5,10 +5,11 @@ Created on Fri Mar 28 19:22:57 2025
 @author: AlejandroPerezS, Huntyboy102
 """
 
-import discord
-import os
 from discord.ext import commands
 from dotenv import load_dotenv
+import discord
+import os
+import spotify_control as spc
 
 # Load environment variables because Github doesn't inject shit
 load_dotenv()
@@ -20,36 +21,61 @@ intents.message_content = True
 # Get the token directly from environment (GitHub will inject it (No it won't))
 TOKEN = os.getenv("DISCORD_TOKEN")
 
-client = discord.Client(intents=intents)
-
 # Create easy to use commands instead of stacking them in on_message function
 bot = commands.Bot(command_prefix='$', intents=intents)
 
-@client.event
+@bot.event
 async def on_ready():
-    print('Bot logged in as {0.user}'.format(client))
+     print('Bot logged in as {0.user}'.format(bot))
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
-        
-    # Help commands for spotify
-    if message.content.startswith('$spotify'):
-        await message.channel.send('I am capable of using the following spotify commands.'
-                                   '\n[$sp_get_current] Get the currently playing track.'
-                                   '\n[$sp_get_previous] Get the previously playing track.'
-                                   '\n[$sp_next] Play the next song in queue.'
-                                   '\n[$sp_previous] Play the previous song'
-                                   '\n[$sp_pause] Pause the currently playing song'
-                                   '\n[$sp_play] Play the currently paused song'
-                                   '\n[$sp_queue] Add song to queue.'
-                                   '\n[$sp_save] Save the current song to "Liked Songs"')
 @bot.command()
-async def spotify_previous(ctx, *, arg):
-    await ctx.send("Fuck you")
+async def spotify(ctx):
+    await ctx.send('I am capable of using the following spotify commands.'
+                               '\n[$sp_get_current] Get the currently playing track.'
+                               '\n[$sp_get_previous] Get the previously playing track.'
+                               '\n[$sp_next] Play the next song in queue.'
+                               '\n[$sp_previous] Play the previous song'
+                               '\n[$sp_pause] Pause the currently playing song'
+                               '\n[$sp_play] Play the currently paused song'
+                               '\n[$sp_queue] Add song to queue.'
+                               '\n[$sp_save] Save the current song to "Liked Songs"')
+        
+@bot.command()
+async def sp_get_current(ctx):
+    current_track = spc.get_current_track()
+    await ctx.send("The current track is: " + str(current_track))
     
-client.run(TOKEN)
+@bot.command()
+async def sp_get_previous(ctx):
+    previous_track = spc.get_previous_track()
+    await ctx.send("The previous track was: " + str(previous_track))
+    
+@bot.command()
+async def sp_next(ctx):
+    spc.next_track()
+    await ctx.send("Skipped track.")
+    
+@bot.command()
+async def sp_previous(ctx):
+    spc.previous_track()
+    await ctx.send("Going to previous track.")
+    
+@bot.command()
+async def sp_pause(ctx):
+    spc.pause()
+    await ctx.send("Track paused.")
+    
+@bot.command()
+async def sp_play(ctx):
+    spc.play()
+    await ctx.send("Track resumed.")
+    
+@bot.command()
+async def sp_queue(ctx):
+    return
+    
+@bot.command()
+async def sp_add(ctx):
+    return
+
+bot.run(TOKEN)
--- spotify_control.py
+++ spotify_control.py
@@ -3,52 +3,98 @@
 Created on Fri Mar 28 19:22:57 2025
 
 @author: hrlov
+These comments are for you Ale
 """
 
 from dotenv import load_dotenv
-from requests import post
-import os
-import base64
-import json
+from spotipy.oauth2 import SpotifyOAuth
+import spotipy
 
 # Load environment variables
 load_dotenv()
 
-client_id = os.getenv("CLIENT_ID")
-client_secret = os.getenv("CLIENT_SECRET")
-
 # URLS can be found throughout https://developer.spotify.com/console/
+AVAILABLE_DEVICES_URL = 'https://api.spotify.com/v1/me/player/devices'
+SPOTIFY_GET_CURRENT_TRACK_URL = 'https://api.spotify.com/v1/me/player/currently-playing'
+NEXT_URL = 'https://api.spotify.com/v1/me/player/next'
+PREVIOUS_URL = 'https://api.spotify.com/v1/me/player/previous'
+PAUSE_URL = 'https://api.spotify.com/v1/me/player/pause'
+PLAY_URL = 'https://api.spotify.com/v1/me/player/play'
+SAVED_TRACKS = 'https://api.spotify.com/v1/me/tracks'
+VOLUME = 'https://api.spotify.com/v1/me/player/volume'
+SPOTIPY_REDIRECT_URI="http://127.0.0.1:8888/callback"
 
-# SPOTIFY_GET_CURRENT_TRACK_URL = 'https://api.spotify.com/v1/me/player/currently-playing'
-# NEXT_URL = 'https://api.spotify.com/v1/me/player/next'
-# PREVIOUS_URL = 'https://api.spotify.com/v1/me/player/previous'
-# PAUSE_URL = 'https://api.spotify.com/v1/me/player/pause'
-# PLAY_URL = 'https://api.spotify.com/v1/me/player/play'
-# RECENT_TRACKS = 'https://api.spotify.com/v1/me/player/recently-played'
-# SAVED_TRACKS = 'https://api.spotify.com/v1/me/tracks'
-# VOLUME = 'https://api.spotify.com/v1/me/player/volume'
-# TOP = '	https://api.spotify.com/v1/me/top/'
-# USER_PLAYLISTS = 'https://api.spotify.com/v1/me/playlists'
+# Get the authentication token for Spotify using Client Authorization Code Flow
+scope = "user-read-playback-state user-modify-playback-state user-read-currently-playing user-read-recently-played"
 
-# Get the authentication token of the Spotify App
-def get_token():
-    auth_string = client_id + ":" + client_secret
-    auth_bytes = auth_string.encode("utf-8")
-    auth_base64 = str(base64.b64encode(auth_bytes), "utf-8")
-    
-    url = "https://accounts.spotify.com/api/token"
-    headers = {
-        "Authorization": "Basic " + auth_base64,
-        "Content-Type": "application/x-www-form-urlencoded"
-        }
-    data = {"grant_type": "client_credentials"}
-    result = post(url, headers=headers, data=data)
-    json_result = json.loads(result.content)
-    token = json_result["access_token"]
-    return token
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
 
-# Make the token viable
-def get_auth_header(token):
-    return {"Authorization": "Bearer " + token}
+# 🎵 Get the current device Spotify is playing on
+def get_device():
+    devices = sp.devices()
+    if "devices" not in devices:
+        print("Error: 'devices' key missing from response.")
+        return None  # Handle missing key gracefully
 
-token = get_token()
+    for device in devices["devices"]:
+        if device["is_active"]:
+            return device["id"]
+
+    print("No active device found.")
+    return None  # No active device found
+
+        
+# Checks which device is currently being used by spotify
+DEVICE_ID = get_device() 
+
+# 🎶 Get the currently playing track
+def get_current_track():
+    current_track = sp.current_playback()
+    if not current_track or "item" not in current_track:
+        print("No track is currently playing.")
+        return None
+
+    track = current_track["item"]
+    track_name = track["name"]
+    artists = ", ".join([artist["name"] for artist in track["artists"]])
+    link = track["external_urls"]["spotify"]
+
+    return {
+        "Title": track_name,
+        "Artist(s)": artists,
+        "Link": link,
+    }
+
+# 🎶 Get the previously played track
+def get_previous_track():
+    results = sp.current_user_recently_played(limit=1)  # Get the last played track
+    if not results or "items" not in results or len(results["items"]) == 0:
+        print("No previously played track found.")
+        return None
+
+    track = results["items"][0]["track"]
+    track_name = track["name"]
+    artists = ", ".join([artist["name"] for artist in track["artists"]])
+    link = track["external_urls"]["spotify"]
+
+    return {
+        "Title": track_name,
+        "Artist(s)": artists,
+        "Link": link,
+    }
+
+def save():
+    return
+
+# 🔀 Playback Controls
+def next_track():
+    sp.next_track(device_id=DEVICE_ID)
+
+def previous_track():
+    sp.previous_track(device_id=DEVICE_ID)
+
+def pause():
+    sp.pause_playback(device_id=DEVICE_ID)
+
+def play():
+    sp.start_playback(device_id=DEVICE_ID)
